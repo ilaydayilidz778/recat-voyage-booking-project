9391fd4bdf15698d49f8f1e7d914c492152fd570
İlayda Yıldız <146078544+ilaydayilidz778@users.noreply.github.com>
2024-05-21 15:47:10 +0300

    Added SCSS properties for the navbar and adjusted layout for screen widths with a max-width of 768px. Integrated the react-icons library into the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import './App.css'
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
+import Main from './Components/Main/Main';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <>
-
+      <Navbar />
+      {/* <Home />
+      <Main />
+      <Footer /> */}
     </>
   )
 }
