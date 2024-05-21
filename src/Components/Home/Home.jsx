008784f54317d57680e291aec5314aca0c2c4065
İlayda Yıldız <146078544+ilaydayilidz778@users.noreply.github.com>
2024-05-22 01:46:19 +0300

    home.jsx editing started

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
-import './home.css'
+import './home.css';
+import video from '../../assets/video.mp4';
+import { GrLocation } from 'react-icons/gr';
 
 const Home = () => {
     return (
-        <div>Home</div>
-    )
+        <section className='home'>
+            <div className='overlay'></div>
+            <video src={video} muted autoPlay loop type='video/mp4'></video>
+            <div className='homeContent container'>
+                <div className='textDiv'>
+
+                    <span className='smallText'>
+                        Our Packages
+                    </span>
+
+                    <h1 className='homeTitle'>
+                        Search Your Holidays
+                    </h1>
+                </div>
+                <div className='cardDiv grid'>
+                    <div className='destinationInput'>
+                        <label htmlFor='city'>Search your destination:</label>
+                        <div className='input flex'>
+                            <input type='text' placeholder='Enter name here...' />
+                            <GrLocation className='icon' />
+                        </div>
+                    </div>
+                    <div className='dateInput'>
+                        <label htmlFor='date'>Select your date:</label>
+                        <div className='input flex'>
+                            <input type='date' />
+                        </div>
+                    </div>
+                    <div className='priceInput'>
+                        <div className="label_total flex">
+                            <label htmlFor='price'>Max Price:</label>
+                            <h3 className='total'>$5000</h3>
+                        </div>
+                        <div className='input flex'>
+                            <input type='range' max='5000' min='1000' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
 
-export default Home
+export default Home;
